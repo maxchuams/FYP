b5ac138c0642bbca25b0c9fbf9e6b2baede12fb3
Lam <kianlam999@gmail.com>
2016-03-06 01:10:21 +0800

    Add method to retrieve on-going project by PM name

--- Codes/RWapp/src/java/src/model/ProjectDAO.java
+++ Codes/RWapp/src/java/src/model/ProjectDAO.java
@@ -69,6 +69,31 @@ public class ProjectDAO {
         return toReturn;
     }
 
+    
+    
+       public static ArrayList<Project> retrieveInProgressByPM(String username) {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        ArrayList<Project> toReturn = new ArrayList<Project>();
+        try {
+            conn = ConnectionManager.getConnection();
+            pstmt = conn.prepareStatement("select projectname, trellokey, description, assignby, duedate, priority, iscomplete, type, psize from project where iscomplete = 0 and assignby=?");
+            pstmt.setString(1,username);
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+
+                toReturn.add(new Project(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getInt(6), rs.getInt(7), rs.getString(8), rs.getInt(9)));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(PersonDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            ConnectionManager.close(conn, pstmt, rs);
+        }
+
+        return toReturn;
+    }
     public static ArrayList<String> retrieveAllTypes() {
 
         Connection conn = null;
